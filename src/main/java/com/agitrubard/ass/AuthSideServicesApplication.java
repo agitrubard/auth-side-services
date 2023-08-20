b5bdf708841249cc973227295b09896142f75f7b
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-08-20 18:14:50 +0300

    Dependencies Have Been Refactored

--- src/main/java/com/agitrubard/ass/AuthSideServicesApplication.java
+++ src/main/java/com/agitrubard/ass/AuthSideServicesApplication.java
@@ -1,4 +1,4 @@
-package com.agitrubard.authsideservices;
+package com.agitrubard.ass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthSideServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthSideServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSideServicesApplication.class, args);
+    }
 
 }
