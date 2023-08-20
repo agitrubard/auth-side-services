47479c636913ea3ac319c1336cc35e3087728b60
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-08-20 17:49:11 +0300

    AuthSide Project Has Been Initialized

---
+++ src/main/java/com/agitrubard/authsideservices/AuthSideServicesApplication.java
@@ -0,0 +1,13 @@
+package com.agitrubard.authsideservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSideServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSideServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agitrubard/authsideservices/AuthSideServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agitrubard.authsideservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSideServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
