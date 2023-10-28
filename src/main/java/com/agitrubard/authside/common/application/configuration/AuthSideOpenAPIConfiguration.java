67aef0cce11ec4d016139fe5b9c07cc11e77feb1
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-10-28 16:39:18 +0300

    Open API Configuration Has Been Created

---
+++ src/main/java/com/agitrubard/authside/common/application/configuration/AuthSideOpenAPIConfiguration.java
@@ -0,0 +1,41 @@
+package com.agitrubard.authside.common.application.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The {@code AuthSideOpenAPIConfiguration} class is a Spring configuration class responsible for defining and configuring the OpenAPI (formerly known as Swagger) documentation for the Auth Side API.
+ *
+ * <p>It creates an OpenAPI bean with information about the API, including the title, version, description, and contact details. This configuration is used to generate interactive API documentation and can be accessed through Swagger UI or other API documentation tools.
+ *
+ * @author Agit Rubar Demir | @agitrubard
+ * @version 1.0.0
+ */
+@Configuration
+class AuthSideOpenAPIConfiguration {
+
+    /**
+     * Creates and configures an OpenAPI bean for the Auth Side API documentation.
+     *
+     * @return An OpenAPI instance containing API metadata.
+     */
+    @Bean
+    public OpenAPI openAPI() {
+
+        final Contact contact = new Contact()
+                .name(" with Agit Rubar Demir")
+                .email("demiragitrubar@gmail.com");
+
+        final Info info = new Info()
+                .title("Auth Side")
+                .version("v1.0.0")
+                .description("Auth Side APIs Documentation")
+                .contact(contact);
+
+        return new OpenAPI().info(info);
+    }
+
+}
