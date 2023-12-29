72937596542ae6313e0271e40289ef1b5628ba55
Agit Rubar Demir <demiragitrubar@gmail.com>
2023-12-29 23:25:27 +0300

    Permission Adapter Layer Has Been Covered with Unit Tests

---
+++ src/test/java/com/agitrubard/authside/auth/adapter/out/persistence/AuthSidePermissionAdapterTest.java
@@ -0,0 +1,87 @@
+package com.agitrubard.authside.auth.adapter.out.persistence;
+
+import com.agitrubard.authside.AuthSideUnitTest;
+import com.agitrubard.authside.auth.adapter.out.persistence.entity.AuthSidePermissionEntity;
+import com.agitrubard.authside.auth.adapter.out.persistence.entity.AuthSidePermissionEntityBuilder;
+import com.agitrubard.authside.auth.adapter.out.persistence.repository.AuthSidePermissionRepository;
+import com.agitrubard.authside.auth.domain.permission.model.AuthSidePermission;
+import com.agitrubard.authside.auth.domain.permission.model.enums.AuthSidePermissionCategory;
+import com.agitrubard.authside.auth.mapper.AuthSidePermissionEntityToPermissionMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+class AuthSidePermissionAdapterTest extends AuthSideUnitTest {
+
+    @InjectMocks
+    private AuthSidePermissionAdapter permissionAdapter;
+
+    @Mock
+    private AuthSidePermissionRepository permissionRepository;
+
+
+    private final AuthSidePermissionEntityToPermissionMapper permissionEntityToPermissionMapper = AuthSidePermissionEntityToPermissionMapper.initialize();
+
+    @Test
+    void whenFindAllPermissions_thenReturnAllPermissions() {
+
+        // Initialize
+        List<AuthSidePermissionEntity> mockPermissionEntities = List.of(
+                new AuthSidePermissionEntityBuilder()
+                        .withId(UUID.randomUUID().toString())
+                        .withName("user:list")
+                        .withCategory(AuthSidePermissionCategory.USER_MANAGEMENT)
+                        .build(),
+                new AuthSidePermissionEntityBuilder()
+                        .withId(UUID.randomUUID().toString())
+                        .withName("role:list")
+                        .withCategory(AuthSidePermissionCategory.ROLE_MANAGEMENT)
+                        .build()
+        );
+
+        Set<AuthSidePermission> mockPermissions = permissionEntityToPermissionMapper.map(mockPermissionEntities);
+
+        // When
+        Mockito.when(permissionRepository.findAll())
+                .thenReturn(mockPermissionEntities);
+
+        // Then
+        Set<AuthSidePermission> permissions = permissionAdapter.findAll();
+
+        Assertions.assertEquals(mockPermissions.size(), permissions.size());
+        Assertions.assertEquals(mockPermissions.iterator().next().getId(), permissions.iterator().next().getId());
+        Assertions.assertEquals(mockPermissions.iterator().next().getName(), permissions.iterator().next().getName());
+        Assertions.assertEquals(mockPermissions.iterator().next().getCategory(), permissions.iterator().next().getCategory());
+
+        Mockito.verify(permissionRepository, Mockito.times(1))
+                .findAll();
+    }
+
+    @Test
+    void whenPermissionsAreNotExist_thenReturnEmptySet() {
+
+        // Initialize
+        List<AuthSidePermissionEntity> mockPermissionEntities = List.of();
+
+        Set<AuthSidePermission> mockPermissions = permissionEntityToPermissionMapper.map(mockPermissionEntities);
+
+        // When
+        Mockito.when(permissionRepository.findAll())
+                .thenReturn(mockPermissionEntities);
+
+        // Then
+        Set<AuthSidePermission> permissions = permissionAdapter.findAll();
+
+        Assertions.assertEquals(mockPermissions.size(), permissions.size());
+
+        Mockito.verify(permissionRepository, Mockito.times(1))
+                .findAll();
+    }
+
+}
