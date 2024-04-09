a5dcf2935695728bb2e1f457b3dd70f27f35f6c3
agitrubard <demiragitrubar@gmail.com>
2024-04-09 23:49:42 +0300

    Usages of `@PreAuthorized` Annotation Have Been Fixed

--- src/main/java/com/agitrubard/authside/auth/adapter/in/web/AuthSidePermissionController.java
+++ src/main/java/com/agitrubard/authside/auth/adapter/in/web/AuthSidePermissionController.java
@@ -6,7 +6,6 @@ import com.agitrubard.authside.auth.domain.permission.model.AuthSidePermission;
 import com.agitrubard.authside.auth.mapper.AuthSidePermissionToPermissionsResponseMapper;
 import com.agitrubard.authside.common.adapter.in.web.response.AuthSideResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +15,6 @@ import java.util.Set;
 /**
  * AuthSidePermissionController is a Spring MVC controller responsible for handling
  * authentication side permission-related HTTP requests within the API version 1.
- * <p>
- * This controller is designed to manage permissions and is secured with
- * role-based authorization for the 'role:add' and 'role:update' authorities.
  *
  * @author Agit Rubar Demir | @agitrubard
  * @version 1.0.0
@@ -38,7 +34,6 @@ class AuthSidePermissionController {
      * @return An AuthSideResponse containing a map of permissions categorized by PermissionCategory.
      */
     @GetMapping("/permissions")
-    @PreAuthorize("hasAnyAuthority('role:add', 'role:update')")
     public AuthSideResponse<Set<AuthSidePermissionsResponse>> findAll() {
         final Set<AuthSidePermission> permissions = permissionUseCase.findAll();
         return AuthSideResponse.successOf(permissionToPermissionsResponseMapper.map(permissions));
