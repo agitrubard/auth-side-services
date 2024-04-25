7436e1f520aac0338e4bfcc2577432f07edb4d1e
agitrubard <demiragitrubar@gmail.com>
2024-04-25 21:06:00 +0300

    `AuthSideListPort` Has Been Removed and `toPageable()` Method Has Been Moved to `AuthSideListing` Class

--- src/main/java/com/agitrubard/authside/auth/adapter/out/persistence/AuthSideRoleReadAdapter.java
+++ src/main/java/com/agitrubard/authside/auth/adapter/out/persistence/AuthSideRoleReadAdapter.java
@@ -6,23 +6,18 @@ import com.agitrubard.authside.auth.adapter.out.persistence.repository.AuthSideR
 import com.agitrubard.authside.auth.application.port.out.AuthSideRoleReadPort;
 import com.agitrubard.authside.auth.domain.role.model.AuthSideRole;
 import com.agitrubard.authside.auth.domain.role.model.AuthSideRolesListing;
-import com.agitrubard.authside.common.application.port.out.AuthSideListPort;
 import com.agitrubard.authside.common.domain.model.AuthSidePage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 /**
- * Adapter class that implements both the {@link AuthSideRoleReadPort} and {@link AuthSideListPort} interfaces.
- * This adapter is responsible for reading authentication side roles and handling listing operations.
- * It interacts with the {@link AuthSideRoleRepository} for data access.
- *
  * @author Agit Rubar Demir | @agitrubard
  * @version 1.0.0
  */
 @Component
 @RequiredArgsConstructor
-class AuthSideRoleReadAdapter implements AuthSideRoleReadPort, AuthSideListPort {
+class AuthSideRoleReadAdapter implements AuthSideRoleReadPort {
 
     private final AuthSideRoleRepository roleRepository;
 
@@ -39,7 +34,7 @@ class AuthSideRoleReadAdapter implements AuthSideRoleReadPort, AuthSideListPort 
 
         final Page<AuthSideRoleEntity> roleEntities = roleRepository.findAll(
                 listing.toSpecification(),
-                this.toPageable(listing.getPagination(), listing.getSort())
+                listing.toPageable()
         );
 
         return AuthSidePage.of(
--- src/main/java/com/agitrubard/authside/common/domain/model/AuthSideListing.java
+++ src/main/java/com/agitrubard/authside/common/domain/model/AuthSideListing.java
@@ -2,6 +2,9 @@ package com.agitrubard.authside.common.domain.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -26,5 +29,29 @@ public abstract class AuthSideListing {
      * Sorting criteria for the listing.
      */
     protected List<AuthSideSorting> sort;
+
+    /**
+     * Converts authentication side listing criteria into a Spring Data's Pageable object.
+     *
+     * @return A Pageable object representing the pagination and sorting criteria.
+     */
+    public Pageable toPageable() {
+
+        if (this.sort != null) {
+            return PageRequest.of(
+                    this.pagination.getPage(),
+                    this.pagination.getPageSize(),
+                    Sort.by(this.sort.stream()
+                            .map(sortable -> Sort.Order.by(sortable.getProperty()).with(sortable.getDirection()))
+                            .toList())
+            );
+        }
+
+        return PageRequest.of(
+                this.pagination.getPage(),
+                this.pagination.getPageSize()
+        );
+
+    }
 
 }
