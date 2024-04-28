057e0a165e5aa8a206aebe4ec5ddf7089951ebdd
agitrubard <demiragitrubar@gmail.com>
2024-04-28 15:04:53 +0300

    Java DOCs Have Been Added to `UUIDs` and `UUIDsValidator` Classes

--- src/main/java/com/agitrubard/authside/common/util/validator/UUIDsValidator.java
+++ src/main/java/com/agitrubard/authside/common/util/validator/UUIDsValidator.java
@@ -7,10 +7,21 @@ import org.apache.commons.collections4.CollectionUtils;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+/**
+ * Validator implementation for the {@link UUIDs} annotation.
+ * Validates if the annotated set of strings contains valid UUIDs.
+ */
 class UUIDsValidator implements ConstraintValidator<UUIDs, Set<String>> {
 
     private static final String PATTERN = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$";
 
+    /**
+     * Validates if the annotated set of strings contains valid UUIDs.
+     *
+     * @param uuids                      the set of strings to validate
+     * @param constraintValidatorContext the context in which the constraint is evaluated
+     * @return {@code true} if all strings are valid UUIDs, {@code false} otherwise
+     */
     @Override
     public boolean isValid(Set<String> uuids, ConstraintValidatorContext constraintValidatorContext) {
 
