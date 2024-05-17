ac0dd8d73a64ff96cfa0dc7f14e7559114742d18
agitrubard <demiragitrubar@gmail.com>
2024-05-17 12:30:07 +0300

    `@GeneratedValue(strategy = GenerationType.IDENTITY)` Annotation Has Been Added to `AuthSideUserEntity.PasswordEntity.id` Field

--- src/main/java/com/agitrubard/authside/auth/adapter/out/persistence/entity/AuthSideUserEntity.java
+++ src/main/java/com/agitrubard/authside/auth/adapter/out/persistence/entity/AuthSideUserEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -111,6 +113,7 @@ public class AuthSideUserEntity extends AuthSideBaseEntity {
          */
         @Id
         @Column(name = "ID")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
 
         /**
