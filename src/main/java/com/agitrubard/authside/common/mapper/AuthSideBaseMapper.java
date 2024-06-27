30123dabd3a55eae90b1b700d3f37746282297a9
agitrubard <demiragitrubar@gmail.com>
2024-06-27 13:50:18 +0300

    Javadoc of `AuthSideBaseMapper` Has Been Upgraded

--- src/main/java/com/agitrubard/authside/common/mapper/AuthSideBaseMapper.java
+++ src/main/java/com/agitrubard/authside/common/mapper/AuthSideBaseMapper.java
@@ -1,49 +1,65 @@
 package com.agitrubard.authside.common.mapper;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
 /**
- * A generic interface for mapping objects of type {@code S} to objects of type {@code T}.
- * This interface provides two methods: one for mapping a single object and another for mapping a collection of objects.
+ * {@link AuthSideBaseMapper} defines methods for mapping between objects of type {@link S} (source)
+ * and {@link T} (target). Implementing classes or interfaces are responsible for providing
+ * implementations for these mapping methods.
  * <p>
- * Implementations of this interface should provide an implementation for the {@link #map(Object)} method that takes an
- * object of type {@code S} and returns an object of type {@code T}. The {@link #map(Collection)} method, which takes
- * a collection of objects of type {@code S}, should be implemented by calling the {@code map} method on each element
- * of the collection and returning a list of the resulting objects of type {@code T}.
+ * This interface supports mapping operations for:
+ * <ul>
+ * <li>Mapping a single source object ({@link S}) to a target object ({@link T}) using {@link #map(Object)}</li>
+ * <li>Mapping a collection of source objects ({@link List<S>}) to a list of target objects ({@link List<T>})
+ * using {@link #map(List)}</li>
+ * <li>Mapping a set of source objects ({@link Set<S>}) to a set of target objects ({@link Set<T>})
+ * using {@link #map(Set)}</li>
+ * </ul>
+ * </p>
  * <p>
- * The purpose of this interface is to provide a generic way of mapping objects from one type to another, allowing for
- * reuse of code and reduction of duplication.
+ * Example usage:
+ * </p>
+ * <pre>{@code
+ * @Mapper
+ * public class AuthSideUserEntityToDomainMapper implements AuthSideBaseMapper<AuthSideUserEntity, AuthSideUser> {
  *
- * @param <S> the type of the source object to be mapped
- * @param <T> the type of the target object to be mapped to
+ *     static AuthSideUserEntityToDomainMapper initialize() {
+ *         return Mappers.getMapper(AuthSideUserEntityToDomainMapper.class);
+ *     }
+ *
+ * }
+ *
+ * }</pre>
+ *
+ * @param <S> the type of the source object to map from
+ * @param <T> the type of the target object to map to
  * @author Agit Rubar Demir | @agitrubard
  * @version 1.0.0
  */
 public interface AuthSideBaseMapper<S, T> {
 
     /**
-     * Maps the specified source object to an object of type {@code T}.
+     * Maps the specified source object to an object of type {@link T}.
      *
      * @param source the source object to be mapped
-     * @return the resulting object of type {@code T}
+     * @return the resulting object of type {@link T}
      */
     T map(S source);
 
     /**
-     * Maps the specified collection of source objects to a list of objects of type {@code T}.
+     * Maps the specified collection of source objects to a list of objects of type {@link T}.
      *
      * @param sources the collection of source objects to be mapped
-     * @return the list of resulting objects of type {@code T}
+     * @return the list of resulting objects of type {@link T}
      */
     List<T> map(List<S> sources);
 
     /**
-     * Maps the specified set of source objects to a set of objects of type {@code T}.
+     * Maps the specified set of source objects to a set of objects of type {@link T}.
      *
      * @param sources the set of source objects to be mapped
-     * @return the set of resulting objects of type {@code T}
+     * @return the set of resulting objects of type {@link T}
      */
     Set<T> map(Set<S> sources);
 
